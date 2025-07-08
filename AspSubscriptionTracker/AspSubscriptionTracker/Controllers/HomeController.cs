1a83775926ea28f4e91ac56eb27be5f19708558f
axe53 <agitty84.ae@gmail.com>
2025-07-08 13:29:52 -0500

    boostrap and boiler plate folders

---
+++ AspSubscriptionTracker/AspSubscriptionTracker/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspSubscriptionTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("home")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("create")]
+        public IActionResult CreateSubscription()
+        {
+            //Bring to Create View (input fields)
+            //Accept input and create Sub Model
+
+            return View("CreateView");
+        }
+
+        [Route("delete")]
+        public IActionResult DeleteSubscription()
+        {
+            return View("DeleteView");
+        }
+    }
+}
--- AspSubscriptionTracker/AspSubscriptionTracker/Program.cs
+++ AspSubscriptionTracker/AspSubscriptionTracker/Program.cs
@@ -5,9 +5,16 @@ namespace AspSubscriptionTracker
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            
+            //Add Controllers
+            builder.Services.AddControllersWithViews();
+            
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            //Enable the components
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseEndpoints(en => { en.MapControllers(); });
 
             app.Run();
         }
